111893c7009576f1d63bc3e8fa39201b88c75d44
kevin-njuguna <kevy.kamah@gmail.com>
2025-07-28 18:14:43 +0300

    add changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
-// src/App.tsx
-import { Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -9,21 +8,22 @@ import DeletedTasksPage from "./pages/DeletedTasksPage";
 import NewTaskPage from "./pages/NewTaskPage";
 import UpdateTaskPage from "./pages/updateTaskPage";
 import UserProfilePage from "./pages/UserProfilePage";
-//import NotFoundPage from "./pages/NotFoundPage";
 import Protected from "./components/Protected";
-import Header from "./components/Header";
+import Header from "./components/Header"; 
+
 
 function App() {
+  
   return (
     <>
-      <Header />
+         <Header/>
       <Routes>
-        {/* Public Routes */}
+        
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
-        {/* Protected Routes */}
+        
         <Route
           path="/tasks"
           element={
@@ -35,45 +35,45 @@ function App() {
         <Route
           path="/completed-tasks"
           element={
-            <Protected>
-              <CompletedTasksPage />
-            </Protected>
+            <Protected><CompletedTasksPage /></Protected>
+              
+           
           }
         />
         <Route
           path="/deleted"
           element={
-            <Protected>
-              <DeletedTasksPage />
-            </Protected>
+            <Protected><DeletedTasksPage /></Protected>
           }
         />
         <Route
           path="/new-task"
           element={
-            <Protected>
-              <NewTaskPage />
-            </Protected>
+            <Protected><NewTaskPage /></Protected>
+              
+           
           }
         />
         <Route
           path="/update/:taskId"
           element={
-            <Protected>
-              <UpdateTaskPage />
-            </Protected>
+            <Protected><UpdateTaskPage /></Protected>
+              
+           
           }
         />
         <Route
           path="/profile"
           element={
-            <Protected>
-              <UserProfilePage />
-            </Protected>
+            <Protected><UserProfilePage /></Protected>
+              
+           
           }
         />
+
+        
       </Routes>
-    </>
+   </>
   );
 }
 
