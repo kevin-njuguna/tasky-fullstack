6b04e2d781c8381d4ffa8d4dd9a2764c15a82118
kevin-njuguna <kevy.kamah@gmail.com>
2025-07-29 07:44:14 +0300

    comment localhost cors origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,6 +11,7 @@ const app = express();
 app.use(
   cors({
     origin: ["https://tasky-fullstack.vercel.app"],
+    //origin: ["http://localhost:5173"],
     methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
     credentials: true,
   }),
